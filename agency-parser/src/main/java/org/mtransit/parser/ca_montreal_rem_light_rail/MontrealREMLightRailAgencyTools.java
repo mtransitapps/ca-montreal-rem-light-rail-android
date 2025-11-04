18d6e2ad3f7654e6aa83924adf6db5ba8b7ce0ab
Mathieu Méa <mathieu.mea@gmail.com>
2025-11-03 21:51:54 -0500

    Compat w/ latest code

--- agency-parser/src/main/java/org/mtransit/parser/ca_montreal_rem_light_rail/MontrealREMLightRailAgencyTools.java
+++ agency-parser/src/main/java/org/mtransit/parser/ca_montreal_rem_light_rail/MontrealREMLightRailAgencyTools.java
@@ -73,15 +73,6 @@ public class MontrealREMLightRailAgencyTools extends DefaultAgencyTools {
 	}
 
 	@Override
-	public @NotNull String getRouteShortName(@NotNull GRoute gRoute) {
-		if (!FeatureFlags.F_USE_GTFS_ID_HASH_INT) {
-			//noinspection DiscouragedApi
-			return gRoute.getRouteId(); // use GTFS ID as route short name // used for GTFS-RT
-		}
-		return super.getRouteShortName(gRoute);
-	}
-
-	@Override
 	public boolean defaultRouteLongNameEnabled() {
 		return true;
 	}
@@ -187,10 +178,6 @@ public class MontrealREMLightRailAgencyTools extends DefaultAgencyTools {
 
 	@Override
 	public @NotNull String getStopCode(@NotNull GStop gStop) {
-		if (!FeatureFlags.F_USE_GTFS_ID_HASH_INT) {
-			//noinspection DiscouragedApi
-			return gStop.getStopId(); // use GTFS ID as stop code // used for GTFS-RT
-		}
 		return EMPTY; // no user facing stop code IRL
 	}
 }
